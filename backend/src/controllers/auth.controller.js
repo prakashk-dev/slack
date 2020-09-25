73ce178a158df93e5f455446a7d7cb0c34fcd1bd
Prakash Kandel <prakashk@outlook.com.au>
2020-09-25 13:00:04 +1000

    Remove /auth route and only use login or signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,75 +30,6 @@ const getUniqueUsername = async (user) => {
   return isUnique();
 };
 
-async function loginOrRegisterUser(req, res) {
-  if (!req.body.email || !req.body.password) {
-    return res.status(422).json({
-      error: "email or password missing",
-    });
-  }
-  const email = new RegExp("^" + req.body.email + "$", "i");
-  try {
-    // use findOne only in this case, as we are using post hook to join the socket
-    let user = await User.findOne({ email: { $regex: email } })
-      .select("+password")
-      .populate("friends.friend")
-      .populate("rooms.room")
-      .populate("groups.group")
-      .exec();
-
-    if (user) {
-      user.comparePassword(req.body.password, function (err, isMatch) {
-        if (isMatch && !err) {
-          user.status = "online";
-          user.save((err) => {
-            if (err) {
-              return res.json({
-                error: "Something went wrong while authenticating you.",
-              });
-            }
-            // if user has friends, send socket info as online
-            sendOnlineStatus(user);
-            // don't send these fields to client
-            user = user.toJSON();
-            delete user.password;
-            // set cookie to the frontend
-            let token = createToken(user);
-            res.cookie(createCookie(token));
-
-            return res.status(200).json({
-              user,
-              token,
-            });
-          });
-        } else {
-          return res.json({
-            error: "Authentication failed, password not match",
-          });
-        }
-      });
-      // if user not exists register
-    } else {
-      const { email, password } = req.body;
-      let user = await User.create({ email, password, status: "online" });
-      // set cookie to the frontend
-      // also include email and hashed password so that when we decode the token
-      // we can verify that the email and password match to our database
-      let token = createToken(user);
-      res.cookie(createCookie(token));
-      user = user.toJSON();
-      delete user.password;
-
-      return res.status(200).json({
-        user,
-        token,
-      });
-    }
-  } catch (err) {
-    logger(err);
-    return res.json({ error: err.message });
-  }
-}
-
 const login = async (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(422).json({
@@ -203,7 +134,7 @@ const signup = async (req, res) => {
   }
 };
 
-export { loginOrRegisterUser, login, signup };
+export { login, signup };
 
 /**
  * See what information should be Included in the json web token
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,6 @@ import express from "express";
 import * as Auth from "../controllers/auth.controller";
 
 const router = express.Router();
-router.route("/").post(Auth.loginOrRegisterUser);
 router.route("/signup").post(Auth.signup);
 router.route("/login").post(Auth.login);
 
